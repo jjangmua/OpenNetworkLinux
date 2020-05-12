1796f09397baef2c5cc1c0e07827169e531f5ae4
jjangmua <jjangmua@celestica.com>
2020-05-12 10:46:27 +0700

    - Update platform name.

---
+++ packages/platforms/ti/armhf/bone/platform-config/r0/src/python/arm_ti_bone_r0/__init__.py
@@ -0,0 +1,9 @@
+from onl.platform.base import *
+from onl.platform.ti import *
+
+class OnlPlatform_arm_ti_bone_r0(OnlPlatformTI):
+    PLATFORM='arm-ti-bone-r0'
+    MODEL="TI Beaglebone Black"
+    SYS_OBJECT_ID=".2"
+    PORT_COUNT=0
+    PORT_CONFIG=""
---
+++ packages/platforms/ti/vendor-config/src/python/ti/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from onl.platform.base import *
+
+class OnlPlatformTI(OnlPlatformBase):
+    MANUFACTURER='TI'
+    PRIVATE_ENTERPRISE_NUMBER=42623
